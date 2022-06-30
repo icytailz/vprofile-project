3757083cfbc3d000387ad2ca9368d6cf30fe3da1
Owl <owl@Owls-Mac-mini.local>
2022-06-30 09:21:44 +0700

    change vagrant file setting

--- vagrant/Automated_provisioning/rabbitmq.sh
+++ vagrant/Automated_provisioning/rabbitmq.sh
@@ -3,11 +3,11 @@ sudo yum install epel-release -y
 sudo yum update -y
 sudo yum install wget -y
 cd /tmp/
-wget http://packages.erlang-solutions.com/erlang-solutions-2.0-1.noarch.rpm
-sudo rpm -Uvh erlang-solutions-2.0-1.noarch.rpm
-sudo yum -y install erlang socat
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash
-sudo yum install rabbitmq-server -y
+wget https://packages.erlang-solutions.com/erlang/rpm/centos/7/x86_64/esl-erlang_24.0.2-1~centos~7_amd64.rpm
+sudo yum -y install esl-erlang*.rpm
+sudo yum -y install socat
+wget https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.8.19/rabbitmq-server-3.8.19-1.el7.noarch.rpm
+sudo yum -y install rabbitmq-server*.rpm
 sudo systemctl start rabbitmq-server
 sudo systemctl enable rabbitmq-server
 sudo systemctl status rabbitmq-server
